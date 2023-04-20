96bdaa9b6bf45bc88b01967b982247204ff01c90
shulyar <andy.shular@gmail.com>
2023-04-20 23:33:57 +0300

    new react project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,6 @@
-import { useEffect, useState } from "react";
+
 
 export const App = () => {
-
-  const [message, setMessage] = useState(null);
-  const [data, setValue] = useState(null);
- 
-  useEffect(()=>{
-      fetch('/api').then((res)=> res.json()).then(res=>setMessage(res.message))
-      fetch('/api/users').then((res)=> res.json()).then(res=>setValue(res.users))
-  }, [])
 
   return (
     <div
@@ -22,15 +14,9 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      <p>
+     
       React homework template
-      </p>
-      <p>
-      {!message ? "......" : message} 
-      </p> 
-      <p>
-      {!data ? "Loading" : data.map(item =><div> {item.email}</div>)}  
-      </p>
+   
     </div>
   );
 };
